13def4e0f1dd87f350e6eadab468ba081bc3750b
Lyall <me@lyall.lol>
2026-03-06 02:38:22 +0000

    fix patches

--- Apply-Patches.sh
+++ Apply-Patches.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd ./Source
-for patch in $(ls ../Patches/*.patch | sort -V); do
+for patch in $(ls Patches/*.patch | sort -V); do
     echo "Applying patch: $patch"
     patch -p1 --forward < "$patch"
 done
